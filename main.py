fe0e1d7c3585d83c645f784f3f2143acd920e206
Himanshu Lohaniwal <lohaniwalhimanshu@gmail.com>
2021-01-16 16:38:33 +0530

    Added High score , made changes in end screen and added restart feature

--- main.py
+++ main.py
@@ -9,6 +9,29 @@ fps = 30
 exit_game = False
 game_over = False
 score = 0
+move = ''
+restart = 0
+high_score = 500
+
+#Start Menu
+'''To be continued later!!!!'''
+
+#end screen
+def gameover():
+    restart = 1
+    screen.fill(white)
+    mytext("Game Over", blue, 500, 250, 100)
+    mytext("Your score is : " + str(score), black, 600, 325, 25)
+    mytext('Press R to restart the game .', black, 550, 350, 25)
+    if score >= 300 and score < 500:
+        mytext('You are a GOOD Player!',black,575 , 375, 25)
+    if score < 300 :
+        mytext('You are a NOOB!', black , 600 , 375,25)
+    if score >= 500 and score < 750:
+        mytext('You are a PRO!', black, 600, 375, 25)
+    if score >= 750:
+        mytext('You are a INSANE!', black, 600, 375, 25)
+
 # displaying score
 def mytext(text , color , x , y , s):
     font = pygame.font.SysFont('areial',s)
@@ -67,93 +90,184 @@ clock = pygame.time.Clock()
 while not exit_game:
 
     if game_over:
-        screen.fill(white)
-        mytext ("Game Over" , blue ,500,250 , 100)
-        mytext("Your score is : " + str(score) , black , 600,325 , 25)
-
+        gameover()
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    restart = 0
+                    game_over = False
+                    snake_x = 50
+                    snake_y = 50
+                    snake_s = 25
+                    vel_x = 0
+                    vel_y = 0
+                    snake_list = []
+                    snake_len = 1
+                    score = 0
+                    fps = 30
+                    food_x = random.randint(150, width - 150)
+                    food_y = random.randint(150, height - 150)
+                    food_s = 25
             if event.type == pygame.QUIT:
                 exit_game = True
 
         clock.tick(fps)
         pygame.display.update()
+
     else:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit_game = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                vel_y = -5
-                vel_x = 0
-            if event.key == pygame.K_DOWN:
-                vel_y = 5
-                vel_x = 0
-            if event.key == pygame.K_LEFT:
-                vel_y = 0
-                vel_x = -5
-            if event.key == pygame.K_RIGHT:
-                vel_y = 0
-                vel_x = 5
 
-        head = []
-        head.append(snake_x)
-        head.append(snake_y)
-        snake_list.append(head)
-        if len(snake_list) > snake_len:
-            del snake_list[0]
+        if restart == 0:
 
-        snake_x = snake_x + vel_x
-        snake_y=snake_y+ vel_y
-        if head in snake_list[:-1]:
-            game_over= True
-        if snake_x<0 or snake_x>width or snake_y<0 or snake_y>height:
-            game_over=True
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit_game = True
 
-        if food_x == snake_list or food_y == snake_list:
-            food_x = random.randint(150, width - 150)
-            food_y = random.randint(150, height - 150)
+            if event.type == pygame.KEYDOWN:
 
-        screen.fill(black)
-        plotsnake(screen,green,snake_list,snake_s)
-        pygame.draw.rect(screen, blue, (food_x, food_y, food_s, food_s))
+                if event.key == pygame.K_UP:
 
-        if score >= 150:
-            mine()
-        if score%100 == 0 and score != 0:
-            power()
+                    if snake_len != 1:
 
-        if abs(snake_x -food_x)<25 and abs(snake_y-food_y)<25 :
-            score = score+10
-            food_x = random.randint(150, width-150)
-            food_y = random.randint(150, height-150)
-            snake_len = snake_len+10
-            fps = fps + 0.5
-            if score >= 150:
-                mine_y , mine_x = random.randint(150, height-150),random.randint(150, width-150)
-            if score%100 == 0 and score != 0:
-                power_y,power_x = random.randint(150, height-150),random.randint(150, width-150)
+                        if move != 'down':
 
-        if score >= 150 :
-            if abs(snake_x - mine_x) < 25 and abs(snake_y - mine_y) < 25:
-                game_over = True
-        if score% 100 == 0 and score != 0:
-            if abs(snake_x - power_x) < 25 and abs(snake_y - power_y) < 25:
-                fps = fps - 1.5
-                score = score + 20
-                mine_y, mine_x = random.randint(150, height - 150), random.randint(150, width - 150)
-                power_y, power_x = random.randint(150, height - 150), random.randint(150, width - 150)
+                            vel_y = -5
+                            vel_x = 0
+                            move = 'up'
+
+                    elif snake_len==1:
+
+                        vel_y = -5
+                        vel_x = 0
+                        move = 'up'
+
+                if event.key == pygame.K_DOWN:
+
+                    if snake_len != 1:
+
+                        if move != 'up':
+
+                            vel_y = 5
+                            vel_x = 0
+                            move = 'down'
+
+                    elif snake_len==1:
+
+                        vel_y = 5
+                        vel_x = 0
+                        move = 'down'
+
+                if event.key == pygame.K_LEFT:
+
+                    if snake_len != 1:
+
+                        if move != 'right':
+
+                            vel_y = 0
+                            vel_x = -5
+                            move = 'left'
+
+                    elif snake_len==1:
+
+                        vel_y = 0
+                        vel_x = -5
+                        move = 'left'
+
+                if event.key == pygame.K_RIGHT:
+
+                    if snake_len != 1:
+
+                        if move != 'left':
+
+                            vel_y = 0
+                            vel_x = 5
+                            move = 'right'
+
+                    elif snake_len==1:
+
+                        vel_y = 0
+                        vel_x = 5
+                        move = 'right'
+
+            head = []
+            head.append(snake_x)
+            head.append(snake_y)
+            snake_list.append(head)
+
+            if len(snake_list) > snake_len:
+                del snake_list[0]
+
+            snake_x = snake_x + vel_x
+            snake_y=snake_y+ vel_y
+
+            if head in snake_list[:-1]:
+                game_over= True
+
+            if snake_x<0 or snake_x>width or snake_y<0 or snake_y>height:
+                game_over=True
+
+            if food_x == snake_list or food_y == snake_list:
                 food_x = random.randint(150, width - 150)
                 food_y = random.randint(150, height - 150)
 
-        if mine_x== food_x and mine_y == food_y:
-            mine_y, mine_x = random.randint(150, height - 150), random.randint(150, width - 150)
-        elif mine_x == power_x and mine_y == power_y:
-            mine_y, mine_x = random.randint(150, height - 150), random.randint(150, width - 150)
+            screen.fill(black)
+            plotsnake(screen,green,snake_list,snake_s)
+            pygame.draw.rect(screen, blue, (food_x, food_y, food_s, food_s))
 
-        mytext("Score : "+ str(score) , white ,5,5,25)
-        mytext("FPS : " + str(fps) , white , 150,5 , 25)
-        clock.tick(fps)
-        pygame.display.update()
+            if high_score == 500:
+                if  high_score - score == 10 or score == high_score :
+                    mytext("You are going to break the HIGH SCORE." , white , 450 ,5,25)
+
+            if score == 510 or score == 520:
+                mytext("You broke the HIGH SCORE !!!",white,450,5,25)
+
+            if score > high_score :
+                high_score = score
+
+            if score >= 150:
+                mine()
+
+            if score%100 == 0 and score != 0:
+                power()
+
+            if abs(snake_x -food_x)<25 and abs(snake_y-food_y)<25 :
+                score = score+10
+                food_x = random.randint(150, width-150)
+                food_y = random.randint(150, height-150)
+                snake_len = snake_len+10
+                fps = fps + 0.5
+
+                if score >= 150:
+                    mine_y , mine_x = random.randint(150, height-150),random.randint(150, width-150)
+
+                if score%100 == 0 and score != 0:
+                    power_y,power_x = random.randint(150, height-150),random.randint(150, width-150)
+
+            if score >= 150 :
+
+                if abs(snake_x - mine_x) < 25 and abs(snake_y - mine_y) < 25:
+                    game_over = True
+
+            if score% 100 == 0 and score != 0:
+
+                if abs(snake_x - power_x) < 25 and abs(snake_y - power_y) < 25:
+                    fps = fps - 1.5
+                    score = score + 20
+                    mine_y, mine_x = random.randint(150, height - 150), random.randint(150, width - 150)
+                    power_y, power_x = random.randint(150, height - 150), random.randint(150, width - 150)
+                    food_x = random.randint(150, width - 150)
+                    food_y = random.randint(150, height - 150)
+
+            if mine_x== food_x and mine_y == food_y:
+                mine_y, mine_x = random.randint(150, height - 150), random.randint(150, width - 150)
+
+            elif mine_x == power_x and mine_y == power_y:
+                mine_y, mine_x = random.randint(150, height - 150), random.randint(150, width - 150)
+
+            mytext("Score : "+ str(score) , white ,5,5,25)
+            mytext("Snake speed : " + str(fps) , white , 125,5 , 25)
+            mytext('High Score : '+str(high_score),white,305,5,25)
+            clock.tick(fps)
+            pygame.display.update()
 
 pygame.quit()
 quit()
